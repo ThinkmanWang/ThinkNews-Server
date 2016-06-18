18782e62c14e259b2ea026bc548f8fe01a185196
Thinkman Wang <thinkman_wang@azhuoinfo.com>
2016-06-18 17:24:23 +0800

    finish upload file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 #coding=utf-8
 
+import sys
+import os
+
 import MySQLdb
 from utils.mysql_python import MysqlPython
 from models.User import User
@@ -11,10 +14,15 @@ from utils.object2json import obj2json
 import hashlib
 import time
 
-from flask import Flask
-from flask import request
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import render_template
+from werkzeug import secure_filename
 
-app = Flask(__name__)
+#app = Flask(__name__) 
+app = Flask(__name__, static_url_path = "/pic", static_folder = "upload")
+app.config['UPLOAD_FOLDER'] = 'upload/'
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 '''
 def main():
@@ -50,6 +58,16 @@ def main():
     szToken = ("%s%d" % ("a0a475cf454cf9a06979034098167b9e", int(time.time())))
     print(hashlib.md5(szToken).hexdigest())
 '''
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+def get_file_ext(filename):
+    if ('.' in filename):
+        return filename.rsplit('.', 1)[1]
+    else:
+        return ""
     
 @app.route("/", methods=['POST', 'GET'])
 def index():
@@ -73,6 +91,21 @@ def login():
         szRet = obj2json(retModel)
             
     return szRet
-    
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['avatar']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            
+            savedFileName = hashlib.md5(filename).hexdigest() + "." + get_file_ext(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], savedFileName ) )
+            return obj2json(RetModel(0, "Success", {}) )
+        else:
+            return obj2json(RetModel(2, "Upload failed", {}) )
+    else:
+        return obj2json(RetModel(2, "Upload failed", {}) )
+        
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', port=8000)
